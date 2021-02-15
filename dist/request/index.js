08caff91d0c4ad7f6c0a9961dccd6d3355d71272
Gideon Kombian <gidkom@yahoo.co.uk>
2021-02-15 02:31:16 +0000

    package name change

--- dist/request/index.js
+++ dist/request/index.js
@@ -5,6 +5,7 @@ const config_1 = require("./../config");
 const request = axios_1.default.create({
     baseURL: config_1.baseUrl,
 });
+axios_1.default.defaults.adapter = require('axios/lib/adapters/http');
 request.interceptors.request.use(req => {
     // console.log(` ${req.method} ${req.baseURL}${req.url}`);
     return req;
